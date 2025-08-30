f2f5523d3a3cd27906375453279d3359458b03f8
vsmeoo <vsmeonline@gmail.com>
2025-08-30 20:26:23 +0530

    fix: correct image source path in Brands component

--- src/components/sections/Brands.tsx
+++ src/components/sections/Brands.tsx
@@ -19,7 +19,7 @@ export const Brands = () => {
                 <div className="flex justify-center flex-wrap gap-4">
                     {logos.map((logo,key) =>(
                         <div key={key} className="p-4 sm:p-5 rounded-xl bg-body border border-box-border">
-                            <img src={`public/assets/${logo}.png`} width="100" height="60" 
+                            <img src={`/assets/${logo}.png`} width="100" height="60" 
                             className="h-7 sm:h-10 w-auto ease-linear duration-150 grayscale hover:!grayscale-0 hover:scale-105"/>
                         </div>
                     ))}
